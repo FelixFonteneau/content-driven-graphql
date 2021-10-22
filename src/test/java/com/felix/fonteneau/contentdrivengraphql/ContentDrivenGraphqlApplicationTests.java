8a3ddba6dda19d0fe30b2f01bcab6e5f9783837f
felixfon <felix.fonteneau@hotmail.com>
2021-10-22 10:53:15 +0200

    Init commit

---
+++ src/test/java/com/felix/fonteneau/contentdrivengraphql/ContentDrivenGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felix.fonteneau.contentdrivengraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentDrivenGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
